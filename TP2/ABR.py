8bb9660a823bccc4849beb83441054573953c9af
NanoClem <clemdecoop.pro@gmail.com>
2018-12-14 02:22:16 +0100

    fin classe ABR

--- TP2/ABR.py
+++ TP2/ABR.py
@@ -16,14 +16,38 @@ class ABR(BNode) :
     """
 
 
-    def __init__(self, label, lChild, rChild, values) :
+    def __init__(self, values, label=None, lChild=None, rChild=None) :
         """
         Constructeur de la classe ABR
-        :param label, lChild, rChild: attributs de la classe parente BNode
+        :param lChild, rChild: attributs de la classe parente BNode
         :param values: liste des etiquettes
         """
-        super().__init__(label, h, lChild, rChild)       # constructeur parent
         self.values = values
+        super().__init__(label, lChild, rChild)       # constructeur parent
+        
+        
+        
+    def __eq__(self, other) :
+        """
+        Surcharge de l'operateur == pour les ABR
+        """        
+        return self.getContent() == other.getContent()
+    
+    
+    
+    def __lt__(self, other) :
+        """
+        Surcharge de l'operateur < pour les ABR
+        """
+        return self.getContent() < other.getContent()
+    
+    
+    
+    def __gt__(self, other) :
+        """
+        Surcharge de l'operateur > pour les ABR
+        """
+        return self.getContent() > other.getContent()
     
     
     
@@ -33,6 +57,24 @@ class ABR(BNode) :
         :return: racine de l'ABR
         """
         return self
+    
+    
+    
+    def isNull(self) :
+        """
+        Verifie si la racine est nulle
+        :return: True si la racine est nulle, False sinon
+        """
+        return self.root() == None
+    
+    
+    
+    def isEmpty(self) :
+        """
+        Verifie si la liste des etiquettes est vide
+        :return: True si la liste est vide, False sinon
+        """
+        return self.values == None
     
     
         
@@ -62,6 +104,15 @@ class ABR(BNode) :
     
     
     
+    def getValues(self) :
+        """
+        Retourne la liste des etiquettes de l'ABR
+        :return: liste des etiquettes
+        """
+        return self.values
+    
+    
+    
     def setLeftChild(self, new_node) :
         """
         Remplace le noeud fils gauche
@@ -82,59 +133,108 @@ class ABR(BNode) :
     
     def rotG(self) :
         """
-        Effectue une rotation a gauche de l'ABR
+        Effectue une rotation a gauche non destructive de l'ABR
+        :return: l'ABR qui a subi une rotation a gauche
         """
+        abr2   = self.rightChild                                                     # sous-ABR egal au fils droit de l'ABR  
+        abr3   = ABR(self.values, self.getContent(), self.leftChild, abr2.leftChild)
+        retABR = ABR(self.values, abr2.getContent(), abr3, abr2.rightChild)          # ABR rotationne a gauche (rassemblement des sous ABR)
         
-        abr2 = self.rightChild
-        
-        abr3 = ABR(self.values)
-        abr3.setLeftChild(self.leftChild)
-        abr3.setContent(self.content)
-        abr3.setRightChild(abr2.leftChild)
-        
-        retABR = ABR(self.values)
-        retABR.setLeftChild(abr3)
-        retABR.setContent(abr2.content)
-        retABR.setRightChild(abr2.rightChild)
+        return retABR
+    
+    
+    
+    def rotD(self) :
+        """
+        Effectue une rotation à droite non destrucitve de l'ABR
+        :return: l'ABR qui a subi une rotation a droite
+        """
+        abr2   = self.leftChild
+        abr3   = ABR(self.values, self.getContent(), abr2.rightChild, self.rightChild)
+        retABR = ABR(self.values, abr2.getContent(), abr2.leftChild, abr3)
         
         return retABR
         
-        
     
+        
+    def balance(self) :
+        """
+        Equilibre l'ABR en effectuant une double rotation (gauche puis droite)
+        si la hateur du petit-fils gauche du noeud courrant est inférieur celle du petit-fils droit, 
+        sinon une simple rotation a droite \n
+        Il n'y a aucune rotation si la difference de la hauteur du fils gauche par celle du fils droit n'est pas egale a 2 ou -2
+        :return: l'ABR equilibre
+        """
+        hL = self.leftChild.getHeight()     # hauteur du fils gauche de l'ABR courrant
+        hR = self.rightChild.getHeight()    # hauteur du fils droit de l'ABR courrant
+        
+        if hL - hR == 2 or hL - hR == -2:
+            
+            if self.leftChild.leftChild.getHeight() < self.leftChild.rightChild.getHeight() :
+                self.leftChild = self.leftChild.rotG()      # rotation a gauche
+            return self.rotD()      # rotation a droite
+        
+        return self.root()
+                
+
     
             
     def add_node(self, label) :
         """
-        Ajoute un noeud dans l'ABR
+        Ajoute un noeud dans l'ABR et l'equilibre
         :param label: etiquette du noeud a ajouter
         """
-        pass
+        if label == None :
+            return self.root()
+#         Si la racine est nulle
+        if self.isNull() :
+            return ABR(self.values, label, None, None)
+        # contenu du noeud courrant superieur
+        if(label < self.getContent()) :
+            self.leftChild = self.leftChild.add_node(label)
+        # contenu du noeud courrant inferieur
+        if(label > self.getContent()) :
+            self.rightChild = self.rightChild.add_node(label)
+            
+        return self.balance()
         
         
     
-    def balance(self) :
+    def makeABR(self) :
         """
-        
+        Fabrique un ABR a partir d'une liste de valeurs
+        :return: ABR construit a partir de sa liste de valeur en attribut
         """
-        pass
-    
-    
-    
-    def makeABR() :
-        """
+        if self.values == None :
+            return self.root()
+                
+        # Construction de l'ABR
+        v = self.values.pop()       # la valeur est enlevee de la liste dans tous les cas
+        if v != None and not self.exist(v) :      # on ajoute le noeud s'il n'existe pas deja
+            self.add_node(v)
+            
+        return self.makeABR()
+            
         
-        """
-        pass
+        
         
     
-    
-    
-    
+
 # =============================================================================
-#  TESTS
+#  MAIN PROGRAM
 # =============================================================================
 
-#abr = ABR()
+if __name__ == "__main__" :
+    
+    intVal  = [1, 5, 9, 7, 6, 4, 8, 3, 2]                           # liste des labels(entiers)
+    charVal = ["il", "est", "2", "h", "a", "laide"]    # liste des labels(char), compares selon la longueur de chaîne
+    
+    intABR = ABR(intVal)
+    intABR = intABR.makeABR()
+    
+    
+    
+    
 
 
 
